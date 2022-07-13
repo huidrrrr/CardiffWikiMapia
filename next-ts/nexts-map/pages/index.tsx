8a56e4b1e0c43709435eeb71458149ad37e43470
Jin Chen <1161529963@qq.com>
2022-07-13 16:59:51 +0100

    Patch for bug(first diectionRender wont disapper)

--- next-ts/nexts-map/pages/index.tsx
+++ next-ts/nexts-map/pages/index.tsx
@@ -5,13 +5,10 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useMemo } from "react";
 import Map from "../components/map";
 
-
-
-
 export default function Home() {
   const position = useMemo(() => ({ lat: 51.4837, lng: -3.1681 }), []);
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyD0EITvU6aSQn9zF8fSXHQ5Dd0MjF5Q7aI",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
     libraries: ["places"],
   });
   if (!isLoaded) {
---
+++ next-ts/nextsmap2/pages/index.tsx
@@ -0,0 +1,12 @@
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "../components/map";
+
+export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyD0EITvU6aSQn9zF8fSXHQ5Dd0MjF5Q7aI",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
