08c11ace6b23366bd912c51696ccdffff3a61ff6
Jin Chen <1161529963@qq.com>
2022-07-14 12:49:52 +0100

    Add Marker by clicking demo

--- NextJS/maptest/pages/GMarker.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-export default function GMarker(props) {
-
-
-  return (
-    <Marker position={props.position}></Marker>
-  )
-}
